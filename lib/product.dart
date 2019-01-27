8ae756b2d824eba74dbeee0ed69ba83b2d3cd93d
Willy Aguirre <marti1125@gmail.com>
2019-01-27 01:17:25 -0500

    se agrega la lista de productos a la venta

--- lib/product.dart
+++ lib/product.dart
@@ -6,24 +6,147 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
           title: new Text('Productos'),
         ),
-        body: _productContent(context)
-    );
+        body: _productContent(context));
   }
 
   Widget _productContent(BuildContext context) {
     return new ListView(
       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
       children: <Widget>[
-        new Text('Productos')
+
+        Center(
+          child: new Card(
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Container(
+                      alignment: FractionalOffset.center,
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
+                      child: new Image.asset(
+                        'assets/fruit.png',
+                        height: 60.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    new Container(
+                      child: new Text(
+                        'Frutas y Vegetales',
+                        style: new TextStyle(
+                          fontSize: 20.0
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('VER PEDIDOS'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        Center(
+          child: new Card(
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Container(
+                      alignment: FractionalOffset.center,
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
+                      child: new Image.asset(
+                        'assets/natural_medicine.png',
+                        height: 60.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    new Container(
+                      child: new Text(
+                        'Medicina Natural',
+                        style: new TextStyle(
+                            fontSize: 20.0
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('VER PEDIDOS'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        Center(
+          child: new Card(
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Container(
+                      alignment: FractionalOffset.center,
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
+                      child: new Image.asset(
+                        'assets/old.png',
+                        height: 60.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    new Container(
+                      child: new Text(
+                        'Huacos',
+                        style: new TextStyle(
+                            fontSize: 20.0
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('VER PEDIDOS'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
       ],
     );
   }
-
 }
